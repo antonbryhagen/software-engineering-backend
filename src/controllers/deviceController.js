0cf34429da15b769d4f4cfdd98fbc353dfaa9b6c
Anton Bryhagen <anton.bryhagen@gmail.com>
2025-03-05 18:02:19 +0100

    Add routes and controller functions to handle devices

---
+++ src/controllers/deviceController.js
@@ -0,0 +1,144 @@
+import bcrypt from "bcrypt";
+import db from "../../models/index.js"
+
+const { Device } = db;
+
+export const addDeviceToDB = async (req, res) => {
+    try {
+        const { device_name, device_type, location, status } = req.body;
+
+        if (!device_name || !device_type || !location) {
+            return res.status(400).json({message: "Missing required data"});
+        } 
+
+        const newDevice = await Device.create({
+            deviceName: device_name,
+            deviceType: device_type,
+            status: status ? status : "off", //default off status, else pass in status wanted
+            lastUpdate: new Date()
+            //location: location,
+        });
+
+        return res.status(201).json({
+            message: "Device added to database",
+            device_id: newDevice.id,
+            device_name: newDevice.deviceName,
+            status: newDevice.status
+        })
+
+    } catch (error) {
+        console.log("Error adding device: ", error);
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+}
+
+export const getAllDevices = async (req, res) => {
+    try {
+
+        //check permssions table for which devices to get
+
+        const devices = await Device.findAll({
+            attributes: ["id", "deviceType", "deviceName", "status", "lastUpdate", "updatedAt", "createdAt"],
+        });
+
+        const formattedDevices = devices.map(device => ({
+            device_id: device.id,
+            device_name: device.deviceName,
+            device_type: device.deviceType,
+            status: device.status,
+            last_update: device.lastUpdate,
+            updated_at: device.updatedAt,
+            created_at: device.createdAt
+        })) 
+
+        res.json(formattedDevices)
+
+    } catch (error) {
+        console.log("Error getting all devices: ", error);
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+}
+
+export const updateDevice = async (req, res) => {
+
+    try {
+        const id = req.params.device_id;
+        const { device_name, device_type, location } = req.body;
+
+        const currentDevice = await Device.findByPk(id);
+
+		if (!currentDevice){
+			return res.status(404).json({ message: "Device not found" });
+		}
+
+        currentDevice.deviceName = device_name || currentDevice.deviceName;
+        currentDevice.deviceType = device_type || currentDevice.deviceType;
+        //currentDevice.location = location || currentDevice.location;
+
+		await currentDevice.save();
+
+        res.json( {message: "Device updated"} );
+
+    } catch (error) {
+        console.log("Error updating device: ", error);
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+}
+
+export const deleteDevice = async (req, res) => {
+	try{
+		const id = req.params.device_id;
+
+		const device = await Device.findByPk(id);
+
+		if (!device){
+			return res.status(404).json({ message: "Device not found" });
+		}
+
+		await device.destroy();
+	
+		res.json({ message: "Device deleted" });
+
+	} catch (error) {
+		console.log("Error updating device: ", error);
+		return res.status(500).json({ message: "Internal Server Error" });
+	}
+	
+}
+
+export const toggleDevice = async (req, res) => {
+    try{
+		const id = req.params.device_id;
+        const newStatus = req.body.status;
+
+		const device = await Device.findByPk(id);
+
+		if (!device) {
+			return res.status(404).json({ message: "Device not found" });
+		}
+
+        if (!newStatus) {
+            return res.status(400).json({ message: "No updated status provided" });
+        }
+
+        //TODO Send message to actual device to toggle
+
+        const messageForDevice = {
+            message_type: "device_update",
+            device_id: parseInt(id),
+            status: newStatus
+        }
+
+        console.log(messageForDevice)
+
+		device.status = newStatus;
+
+		await device.save();
+	
+		res.json({ device_id: device.id, status: newStatus });
+
+	} catch (error) {
+		console.log("Error updating device: ", error);
+		return res.status(500).json({ message: "Internal Server Error" });
+	}
+}
--- src/routes/deviceRoutes.js
+++ src/routes/deviceRoutes.js
@@ -1,9 +1,20 @@
 import express from "express";
+import jwtValidator from "../middleware/jwtValidator.js";
+
+import { getAllDevices,	addDeviceToDB, updateDevice, deleteDevice, toggleDevice } from "../controllers/deviceController.js";
+
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.json({"message": "api is running"});
-})
+router.post("/", jwtValidator, addDeviceToDB )
+
+router.get("/", jwtValidator, getAllDevices)
+
+router.put("/:device_id", jwtValidator, updateDevice)
+
+router.delete("/:device_id", jwtValidator, deleteDevice)
+
+router.post("/:device_id/toggle", jwtValidator, toggleDevice)
+
 
 export default router;
